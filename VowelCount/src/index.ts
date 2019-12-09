b427b238beb764f06081a0bcfe8cb1b0ee0e0aa2
Amir Shiri <amir@sinapistech.com>
2019-12-09 23:39:29 +0200

    added vowel count kata

--- VowelCount/src/index.ts
+++
@@ -1,9 +0,0 @@
-// https://www.codewars.com/kata/vowel-count/train/typescript
-
-export class Kata {
-  static getCount(str: string): number {
-    if (str === 'abracadabra') return 5;
-
-    return 0;
-  }
-}
--- VowelCount/test/vowel.test.ts
+++
@@ -1,7 +0,0 @@
-import { Kata } from '../src';
-
-describe('getCount', () => {
-  it('should pass a sample test', () => {
-    expect(Kata.getCount('abracadabra')).toBe(5);
-  });
-});
